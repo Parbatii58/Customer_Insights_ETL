26b4602f96aeb4694ce64c7a438855d44ce42dbc
Parbati Thapa <parbatithapamagar001@gmail.com>
2025-10-03 16:34:57 +0545

    Update Inventory status

--- SQLQuery3_InventoryValidaation_and_StockAdjustment.sql
+++ SQLQuery3_InventoryValidaation_and_StockAdjustment.sql
@@ -8,6 +8,17 @@ CREATE TABLE Inventory
 );
 
 
+
+CREATE TABLE Inventory_updated
+(
+	ProductId INT,
+	StockQuantity INT,
+	ReorderLevel INT,
+	Stock_Status VARCHAR(50)
+);
+
+
+
 -- To get Inventory report in excel file
 SELECT i.ProductId,
 	   p.ProductName,
@@ -26,9 +37,24 @@ ON i.ProductId = p.ProductID
 GROUP BY i.StockQuantity, p.Category, i.ProductId, i.ReorderLevel, p.ProductName
 
 
+-- to populate dbo.Inventory_updated
+SELECT i.ProductId,
+	   (i.StockQuantity- SUM(s.Quantity)) AS StockQuantity, 
+	   i.ReorderLevel, 
+	   CASE WHEN (i.StockQuantity- SUM(s.Quantity)) > i.ReorderLevel THEN 'Sufficient Stock'  
+			WHEN (i.StockQuantity- SUM(s.Quantity)) <= i.ReorderLevel AND (i.StockQuantity- SUM(s.Quantity)) >0 THEN 'Insufficient Stock' 
+			WHEN (i.StockQuantity- SUM(s.Quantity)) =0 THEN 'Out of Stock' 
+		END AS 'Stock_Status'
+FROM Sales s
+INNER JOIN Inventory i
+ON s.ProductID = i.ProductId
+INNER JOIN Products p
+ON i.ProductId = p.ProductID
+GROUP BY i.StockQuantity, p.Category, i.ProductId, i.ReorderLevel, p.ProductName
 
 
 SELECT * FROM Customer
 SELECT * FROM Sales
 SELECT * FROM Products
 SELECT * FROM Inventory
+SELECT * FROM Inventory_updated
